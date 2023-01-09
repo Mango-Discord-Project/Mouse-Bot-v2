2f281f9199893918a60329e9bff7f8a75f42e31d
EvanHsieh0415 <evan940415@gmail.com>
2023-01-09 15:41:36 +0800

    feat: main

--- src/main/main.py
+++ src/main/main.py
@@ -1,16 +1,39 @@
+import tomllib
 from os import path, listdir
 
 from dotenv import dotenv_values
+from discord import (
+    Intents,
+    Bot as _Bot)
+from rich import console as _rich_console
 
-from packages import core
 from packages.namespace_pack import *
 
-class Bot(core.BotBase):
+class Bot(_Bot):
     def __init__(self):
-        self.log('Bot object build')
-        super().__init__()
+        intents = Intents.all()
+        intents.message_content = True
+        super().__init__(intent=intents)
+        self.console = _rich_console.Console()
         
-        self.load_extensions(self.get_extension_list())
+        self.log('Bot object build')
+        with open(path.join(*('.', 'src', 'main', 'config', 'bot.toml')), 'rb') as file:
+            self.config = tomllib.load(file)
+        
+        self.load_extensions(*self.get_extension_list())
+    
+    def log(self, message: str) -> str:
+        format_message = f'Main > {message}'
+        self.console.log(format_message)
+        return format_message
+    
+    def generate_path(self, sub_path: list[str]) -> str:
+        return path.join(*(self.config['path']['base'] + sub_path))
+    
+    def get_extension_list(self) -> list[str]:
+        filter_func = lambda filename: filename.endswith('.py')
+        map_func = lambda filename: f"extensions.{filename.removesuffix('.py')}"
+        return map(map_func, filter(filter_func, listdir(path.join(self.generate_path(self.config['path']['extensions'])))))
     
     async def on_ready(self):
         self.log('Bot is ready')
