8736dd914eb0b1a5f605f6454f1a0214951e1c8f
EvanHsieh0415 <evan940415@gmail.com>
2023-01-09 15:40:43 +0800

    feat: config mixin

---
+++ src/main/packages/config_mixin.py
@@ -0,0 +1,30 @@
+import json
+from os import listdir, path
+
+__all__ = ['get_setting']
+
+base_path = ['.', 'src', 'main']
+
+with open(path.join(*[*base_path, 'config', 'commands', 'settings.json'])) as file:
+    command_setting = json.load(file)
+
+lang_premix = {}
+_lang_base_path = [*base_path, 'config', 'lang']
+for filename in listdir(path.join(*_lang_base_path)):
+    lang = filename.removesuffix('.json')
+    with open(path.join(*[*_lang_base_path, filename]), encoding='utf8') as file:
+        _d = json.load(file)
+    for k, v in _d.items():
+        key, type = k.split('.')
+        key, type = '.'.join(key), f'{type}_localizations'
+        
+        lang_premix.setdefault(key, {})
+        lang_premix[key].setdefault(type, {})
+        
+        # lang_premix[key] = lang_premix.get(key, {})
+        # lang_premix[key][type] = lang_premix.get(type, {})
+        
+        lang_premix[key][type] = v
+
+def get_setting(key: str = 'command.base') -> dict:
+    return command_setting.get(key, {}) | lang_premix.get(key, {})
